8e112ec3e5bcbe908264adf38ee4370c8affd73f
nimit aggarwal <aggarwal.nimit5@gmail.com>
2018-07-24 09:11:55 +0530

    weather project

---
+++ weather/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import './index.css';
+
+import App from './components/app';
+
+ReactDOM.render(
+    <Provider store={createStore(rootReducer)}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
